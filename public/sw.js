9ee869037c5d52fbff5791de21c67054418c96e3
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-14 19:02:18 +0100

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,18 @@
 self.addEventListener("install", (event) => {
+  console.log("Service Worker Installing...");
   self.skipWaiting();
 });
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated.");
+  event.waitUntil(clients.claim()); // Ensure service worker takes control of the page immediately
+});
+
 self.addEventListener("notificationclick", (event) => {
-    event.notification.close();
-    event.waitUntil(clients.openWindow("/admin")); 
-  });
+  event.notification.close();
+  event.waitUntil(clients.openWindow("/admin"));
+});
+
 self.addEventListener('push', (event) => {
   const options = {
     body: event.data ? event.data.text() : 'New notification',
@@ -16,6 +24,7 @@ self.addEventListener('push', (event) => {
     self.registration.showNotification('ASHE™', options)
   );
 });
+
 self.addEventListener("message", (event) => {
   if (event.data.type === "TRIGGER_NOTIFICATION") {
     const { title, options } = event.data;
