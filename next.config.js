ebff43dd6e8a4d2be77dc0e8d3fed8c0909c5e5a
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-15 17:27:23 +0100

    dz

--- next.config.js
+++ next.config.js
@@ -31,34 +31,10 @@ const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: false,
 
-  // PWA Configuration
-  pwa: {
-    disable: isDev, // Disable PWA in development mode
-    dest: 'public', // Destination folder for PWA files
-    sw: 'service-worker.js', // Custom service worker file
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/.*\.(?:png|jpg|jpeg|gif|svg|webp|ico|woff2?|ttf|eot|otf|json|html|js|css|map)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'assets-cache',
-          expiration: {
-            maxAgeSeconds: 60 * 60 * 24 * 30, // Cache assets for 30 days
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/your-api-url\.com\/api\//, // Replace with your API URL
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'api-cache',
-          expiration: {
-            maxAgeSeconds: 60 * 60 * 24, // Cache API responses for 1 day
-          },
-        },
-      },
-    ],
-  },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = withPWA({
+  disable: isDev, // Disable PWA in development mode
+  dest: 'public', // Destination folder for PWA files
+  sw: 'service-worker.js',   // Service worker file
+})(nextConfig);
