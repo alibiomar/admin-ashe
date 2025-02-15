971be4394b00823f53917d9b6b8679ef41df2f2c
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-15 02:32:31 +0100

    dad

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <title>ASHE™ Admin</title>
 
         {/* PWA and SEO meta tags */}
-        <link rel="manifest" href="../public/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="application-name" content="ASHE™ Admin" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
