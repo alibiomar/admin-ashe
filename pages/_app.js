4d6e1919580660e3b38c11d639569d02d8145ae4
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-15 04:18:42 +0100

    da

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,13 @@
+// pages/_app.js
 import { useEffect } from 'react';
 import { AuthProvider } from "../contexts/authContext";
-import { generateToken } from "../lib/firebaseClient";
+import {generateToken} from "../lib/firebaseClient";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Ensure the code is running on the client-side (browser) and handle async call
-    if (typeof window !== "undefined") {
-      const initializeToken = async () => {
-        try {
-          await generateToken(); // If it's an async function
-        } catch (error) {
-          console.error("Error generating token:", error);
-        }
-      };
-
-      initializeToken();
-    }
-  }, []); // Empty dependency array, ensures it runs only once when the component mounts
+useEffect(() => {
+    generateToken();
+  }, []);
 
   return (
     <AuthProvider>
@@ -25,5 +15,6 @@ function MyApp({ Component, pageProps }) {
     </AuthProvider>
   );
 }
+
 
 export default MyApp;
